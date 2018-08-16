71354a2fa8e0de124cacea36f40e269d572cc73e
qipaqiu <36105719+zhaofengqiu@users.noreply.github.com>
2018-08-17 05:00:51 +0800

    Update config.py

--- new_hork_time/new_hork_time_flask/config.py
+++ new_hork_time/new_hork_time_flask/config.py
@@ -1,13 +1,14 @@
 SECRET_KEY='a secret key'
 DEBUG = True
-HOSTNAME='xxx'
-PORT    ='xxx'
-DATABASE='xxx'
-USERNAME='xxx'
-PASSWORD='xxx'
+HOSTNAME=''
+PORT    =''
+DATABASE=''
+USERNAME=''
+PASSWORD=''
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 DB_URI  ='mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,
         HOSTNAME,PORT,DATABASE)
 SQLALCHEMY_DATABASE_URI=DB_URI
 
+MAIL_PATH = ''
 
